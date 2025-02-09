af7cfebfdcd6743d87c1ea485285e7f696192db2
aliyousefi14 <158446711+aliyousefi14@users.noreply.github.com>
2025-02-09 05:06:29 -0800

    Create Animation.js

---
+++ src/components/Animation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Animation.css';
+
+function Animation() {
+  return (
+    <div className="animation-container">
+      <div className="balloon">🎈</div>
+      <div className="cake">🎂</div>
+    </div>
+  );
+}
+
+export default Animation;
